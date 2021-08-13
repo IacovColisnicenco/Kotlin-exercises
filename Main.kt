e29f7dbce22ac4b6bf28843bbb6d13e4f2eca354
Iacov Colisnicenco <jacobwebm@gmail.com>
2021-08-13 19:47:14 +0300

    first commit

---
+++ Main.kt
@@ -0,0 +1,25 @@
+fun main(args: Array<String>) {
+    //Златопольский, 1400 задач по программированию, задача: 2.34
+    val v1: Int //data input from user
+    val v2: Int //data input from user
+    val s: Int //data input from user
+    val t: Int //calc value
+
+    // Read the input value.
+    println("Hello, enter V1: ")
+     v1 = readLine()!!.toInt()
+    println("Please, enter V2: ")
+    v2 = readLine()!!.toInt()
+    println("Please enter S: ")
+    s = readLine()!!.toInt()
+
+    val vSblijenia = v1 + v2
+    t = s / vSblijenia
+
+
+//попробую сначало решить задачку без фунции потом напишу функцию.
+
+    println("The time, when the cars will meeting: ${t} ")
+
+
+}
